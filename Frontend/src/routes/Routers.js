01b66892e161a6c8e59b687c9548e792789b8792
adirn26 <adirn26@gmail.com>
2022-11-25 06:11:41 +0530

    Adding admin page

--- Frontend/src/routes/Routers.js
+++ Frontend/src/routes/Routers.js
@@ -9,6 +9,12 @@ import Checkout from "../pages/Checkout";
 import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import List from "../pages/list/List";
+import Single from "../pages/single/Single";
+import New from "../pages/new/New";
+import { productInputs, userInputs } from "../formSource";
+import AdminHome from "../pages/home/AdminHome";
+
 
 const Routers = () => {
   return (
@@ -22,6 +28,26 @@ const Routers = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/admin">
+        <Route index element={<AdminHome />} />
+        <Route path="login" element={<Login />} />
+        <Route path="users">
+          <Route index element={<List />} />
+          <Route path=":userId" element={<Single />} />
+          <Route
+            path="new"
+            element={<New inputs={userInputs} title="Add New User" />}
+          />
+        </Route>
+        <Route path="products">
+          <Route index element={<List/>} />
+          <Route path=":productId" element={<Single/>} />
+          <Route
+            path="new"
+            element={<New inputs={productInputs} title="Add New Product" />}
+          />
+        </Route>
+      </Route>
     </Routes>
   );
 };
